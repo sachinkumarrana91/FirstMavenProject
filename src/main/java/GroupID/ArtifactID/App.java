1cc91c2c56dfdc0f211e54df9702c13cabb63d5a
John Doe <john@sample.com>
2016-07-08 09:30:30 +0530

    First Commit

---
+++ src/main/java/GroupID/ArtifactID/App.java
@@ -0,0 +1,13 @@
+package GroupID.ArtifactID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
